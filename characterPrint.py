b18bf7b5b8573587f4c24794355f16376c6acb3d
FelipeRCMarques <37280474+FelipeRCMarques@users.noreply.github.com>
2021-07-03 18:39:55 -0300

    desenha os caracteres do csv

---
+++ characterPrint.py
@@ -0,0 +1,14 @@
+from Csv_maneger import Csv_maneger
+
+class characterPrint:
+    data = Csv_maneger.read_csv('caracteres-limpo.csv')
+    for j in range(21):
+        for i in range(64):
+            if(i%7==0):
+                print()
+            if(data[j][i] == "1"):
+                print('*', end = "")
+            else:
+                print(' ',end = "")
+        print("\n--------")
+        
