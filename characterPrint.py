22786affb081ad76483acf682b9706599c503574
Lenkiro <50025952+lenkiro@users.noreply.github.com>
2021-07-04 13:37:55 -0300

    Agora o characterPrint é uma função
    Pode ser chamada por outros arquivos, dando como parâmetros a linha e o csv de qual se quer desenhar, e retornando uma matriz como resposta

--- characterPrint.py
+++ characterPrint.py
@@ -1,15 +1,34 @@
-from Csv_maneger import Csv_maneger
+from Csv_manager import Csv_manager
 
 class characterPrint:
-    data = Csv_maneger.read_csv('caracteres-limpo.csv')
+    def draw(linha,name_of_file):
+        #Transforma os dados de uma linha dos arquivos csv numa matriz 7 por 9 para ficar melhor representado
+        #Os '1's viram asteriscos e o resto vira espaço em branco
+        data = Csv_manager.read_csv(name_of_file)
+        contador = 0
+        desenho = []
+        for j in range (9):
+            arr = [0,0,0,0,0,0,0]
+            for i in range(7):
+                if(data[linha][contador] == "1"):
+                    arr[i] = '*'
+                else:
+                    arr[i] = ' '
+                contador += 1
+            desenho.append(arr)
+        return desenho
+
+    #Um exemplo de implementação para desenhar essa matriz seria:
+    #matriz = draw(0,'caracteres-limpo.csv')
+    #for j in range (9):
+    #    for i in range(7):
+    #        print(matriz[j][i],end="")
+    #    print()
+
     
-    for j in range(21):
-        for i in range(63):
-            if(i%7==0):
-                print()
-            if(data[j][i] == "1"):
-                print('*', end = "")
-            else:
-                print(' ',end = "")
-        print("\n--------")
+
+    
+
+
+
         
